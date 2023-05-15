65d9c10dfd008f7dbc199fb68ca71b34f2b27708
KCushUX <124751865+KCushUX@users.noreply.github.com>
2023-05-15 19:03:47 -0400

    Uploaded existing code files

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+console.log("Thanks for coming!");
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
+
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    2000
+  );
+});
+
+$("#click_advance").click(function () {
+  $("#display_advance").toggle("1000");
+  $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
+
+// Back to top shortcut:
+let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
